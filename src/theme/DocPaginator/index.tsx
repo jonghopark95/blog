b981b8fd7fae99ea0b63eb24b16693cd7d370bb0
pumpkiinbell <pumpkiinbell@gmail.com>
2024-10-16 09:18:06 +0900

    이력서 파일 추가

--- src/theme/DocPaginator/index.tsx
+++ src/theme/DocPaginator/index.tsx
@@ -2,15 +2,8 @@ import React, { ComponentProps } from 'react';
 import DocPaginator from '@theme-original/DocPaginator';
 import type DocPaginatorType from '@theme/DocPaginator';
 
-import Utterance from '@site/src/components/Utterance';
-
 type Props = ComponentProps<typeof DocPaginatorType>;
 
 export default function DocPaginatorWrapper(props: Props) {
-  return (
-    <>
-      <DocPaginator {...props} />
-      <Utterance style={{ marginTop: '50px' }} />
-    </>
-  );
+  return <DocPaginator {...props} />;
 }
